30bb20af44981c5d61a6fcaac10a27cbab1984dd
Benja <natanael.96.01@gmail.com>
2022-11-11 17:06:24 -0300

    db models Genre y Videogame listos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -3,10 +3,33 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
+  sequelize.define('Videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
     },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },{
+    timestamps: false,
   });
 };
---
+++ api/src/routes/controllers/VideoController.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+const { Videogame, Genre } = require('../../db');
+
+module.exports = {
+
+};
