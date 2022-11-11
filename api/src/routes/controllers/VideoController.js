a1fa1c11717885cf33c8c4ef8cc3aa8fefcbec86
Benja <natanael.96.01@gmail.com>
2022-11-11 20:17:13 -0300

    ruta del back /videogames listo

--- api/src/routes/controllers/VideoController.js
+++ api/src/routes/controllers/VideoController.js
@@ -1,6 +1,71 @@
 const axios = require('axios');
 const { Videogame, Genre } = require('../../db');
+const { API_KEY } = process.env;
 
 module.exports = {
+	getVideogamesApi: async ()=>{
+		try{
+			let allVideogames = []
+			let videogames = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}`);
+			let next = videogames.data.next;
+			videogames = videogames.data; 
+			videogames = videogames.results.map(videogame => {
+				return {
+					api: true,
+					id: videogame.id,
+					name: videogame.name,
+					Genre: videogame.genres,
+					img: videogame.background_image,
+				};
+			});
+			allVideogames = allVideogames.concat(videogames);
+			for(let i = 0; i<4; i++){
+				videogames = await axios.get(next);
+				next = videogames.data.next;
+				videogames = videogames.data; 
+				videogames = videogames.results.map(videogame => {
+					return {
+						api: true,
+						id: videogame.id,
+						name: videogame.name,
+						Genre: videogame.genres,
+						img: videogame.background_image,
+					};
+				});
+				allVideogames = allVideogames.concat(videogames);
+			}
+			return allVideogames;
+		} catch(err){
+			throw new Error(err.message);
+		}
+	},
 
+	getVideogamesDB: async ()=>{
+		try {
+			let videogames = await Videogame.findAll({
+				attributes: ['name','id'],
+				include:{
+					model: Genre,
+					attributes: ['name'],
+					through: {
+						attributes: []
+					}
+				},
+			});
+			return videogames;
+		} catch(err){
+			throw new Error(err.message);
+		}
+	},
+
+	getVideogamesByName: (name, videogames)=>{
+		name = name.toUpperCase();
+		let match = videogames.filter(videogame => {
+			let videoName = videogame.name.toUpperCase();
+			return videoName.includes(name);
+		});
+
+		if(!match.length) throw new Error("Videogame not found");
+		return match;
+	},
 };
--- api/src/routes/videogames.js
+++ api/src/routes/videogames.js
@@ -1,7 +1,25 @@
 const { Router } = require('express');
+const {
+	getVideogamesApi, 
+	getVideogamesDB,
+	getVideogamesByName, } = require('./controllers/VideoController');
 const router = Router();
 
 router.get("/", async (req,res) => {
+	const { name } = req.query;
+	try{
+		const videogamesApi = await getVideogamesApi();
+		const videogamesDB = await getVideogamesDB();
+		let videogames = [...videogamesDB, ...videogamesApi];
+		if(name){
+			
+			return res.status(200).json(getVideogamesByName(name,videogames));
+		}
+		
+		return res.status(200).json(videogames);
+	}catch(err){
+		res.status(404).json({error: err.message});
+	}
 	
 });
 
