d7cf6146fdf5c5b4f9dc7421413696638b95a7d3
Benja <natanael.96.01@gmail.com>
2022-11-14 21:10:32 -0300

    ruta /genres del back listo

--- api/src/routes/controllers/genreController.js
+++ api/src/routes/controllers/genreController.js
@@ -4,17 +4,21 @@ const { API_KEY } = process.env;
 
 module.exports = {
 	getGenres: async () => {
-		let genres = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
-		genres = genres.data.results.map(genre => {
-			return {name: genre.name}
-		});
-		let genresDB = [];
-		for(let genre = genres){
-			[genre] = await Genre.findOrCreate({
-				where: {name: genre.name}
+		try{
+			let genres = await axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`);
+			genres = genres.data.results.map(genre => {
+				return {name: genre.name}
 			});
-			genresDB.push(genre);
+			let genresDB = [];
+			for(let genre of genres){
+				[genre] = await Genre.findOrCreate({
+					where: {name: genre.name}
+				});
+				genresDB.push(genre);
+			}
+			return genresDB;
+		} catch(err){
+			throw new Error(err.message);
 		}
-		return genresDB;
 	},
 };
--- api/src/routes/genres.js
+++ api/src/routes/genres.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
-
+const { getGenres } = require('./controllers/genreController');
 const router = Router();
 
 router.get("/", async (req,res) => {
-
+	try{
+		res.status(200).json(await getGenres());
+	}catch(err){
+		res.status(404).json({error: err.message});
+	}
 });
 
 module.exports = router;
